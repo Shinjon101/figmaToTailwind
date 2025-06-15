67949647d93a57de9e980f911739bf6669d36eff
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2025-06-15 14:08:57 +0530

    Refactor: Hero Text Wrapping

--- Components/LandingPageComponents/Hero.tsx
+++ Components/LandingPageComponents/Hero.tsx
@@ -4,13 +4,14 @@ const Hero = () => {
   return (
     <>
       <section className="bg-white w-full flex justify-center pt-30">
-        <div className="lg:min-w-[860px] min-h-[970px] sm:grid-cols-2 grid-cols-2">
-          <div className=" flex flex-col items-center ">
-            <h1 className="font-bold text-4xl md:text-6xl mb-10">
-              Awesome Title
+        <div className="max-w-6xl mx-auto lg:min-w-[860px] min-h-[970px] sm:grid-cols-2 grid-cols-2">
+          <div className=" flex flex-col items-center tems-start text-left lg:max-w-[850px]">
+            <h1 className="font-bold text-center text-4xl md:text-6xl mb-10 leading-tight md:leading-tight">
+              Get things done by awesome remote team
             </h1>
-            <h2 className="text-gray-500 font-bold text-xs md:text-sm">
-              This is a great place to describe your product and what it does.
+            <h2 className="text-gray-500 text-center font-bold text-xs md:text-sm lg:max-w-[425px]">
+              We share common trends and strategies for improving your rental
+              income and making sure you stay in high demand.
             </h2>
             <Button
               onClick={() => console.log("meow")}
